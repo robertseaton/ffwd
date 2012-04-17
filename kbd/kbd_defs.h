97e052ce32da8dc195d4faeb60c9596df2d1b03d
robert seaton <robb@os-blog.com>
2012-04-16 20:38:04 -0500

    Press "f" for fullscreen.

--- kbd/kbd_defs.h
+++ kbd/kbd_defs.h
@@ -3,6 +3,7 @@
 #ifndef KBD_DEFS
 
 #define KBD_QUIT "q"
+#define KBD_FULLSCREEN "f"
 
 #define KBD_DEFS
 #endif
--- kbd/kbd_x11.c
+++ kbd/kbd_x11.c
@@ -17,6 +17,23 @@ extern pthread_mutex_t x11mutex;
 extern int height;
 extern int width;
 
+void x11_fullscreen() {
+     XEvent xev;
+     Atom wm_state = XInternAtom(d, "_NET_WM_STATE", False);
+     Atom fullscreen = XInternAtom(d, "_NET_WM_STATE_FULLSCREEN", False);
+
+     memset(&xev, 0, sizeof(xev));
+     xev.type = ClientMessage;
+     xev.xclient.window = window;
+     xev.xclient.message_type = wm_state;
+     xev.xclient.format = 32;
+     xev.xclient.data.l[0] = 1;
+     xev.xclient.data.l[1] = fullscreen;
+     xev.xclient.data.l[2] = 0;
+
+     XSendEvent(d, DefaultRootWindow(d), False, SubstructureNotifyMask, &xev);
+}
+
 void x11_event_loop() {
      XEvent ev;
      char string[25];
@@ -40,6 +57,8 @@ void x11_event_loop() {
                if (strcmp(string, KBD_QUIT) == 0) {
                     XCloseDisplay(d);
                     exit(0);
+               } else if (strcmp(string, KBD_FULLSCREEN) == 0) {
+                    x11_fullscreen();
                }
                break;
           case ConfigureNotify:
